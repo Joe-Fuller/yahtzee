a8a29f4151b13950bf8791732ccfa25389b66166
Joe <joefuller042@gmail.com>
2023-06-07 12:33:17 +0100

    create scorecard.js

---
+++ components/scorecard.js
@@ -0,0 +1,21 @@
+export default function Scorecard() {
+  return (
+    <div>
+      <p>Score</p>
+      <p>aces</p>
+      <p>twos</p>
+      <p>threes</p>
+      <p>fours</p>
+      <p>fives</p>
+      <p>sixes</p>
+      <p>bonus</p>
+      <p>3 of a kind</p>
+      <p>4 of a kind</p>
+      <p>full house</p>
+      <p>small straight</p>
+      <p>large straight</p>
+      <p>yahtzee</p>
+      <p>straight</p>
+    </div>
+  );
+}
