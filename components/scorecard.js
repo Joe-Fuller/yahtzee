765ee1418a48615ee7e14d9438c1f4cb919c89a9
Joe <joefuller042@gmail.com>
2023-06-07 12:40:10 +0100

    update scorecard to use category.js

--- components/scorecard.js
+++ components/scorecard.js
@@ -1,21 +1,23 @@
+import Category from "./category";
+
 export default function Scorecard() {
   return (
     <div className="text-center">
       <p>Score</p>
-      <p>aces</p>
-      <p>twos</p>
-      <p>threes</p>
-      <p>fours</p>
-      <p>fives</p>
-      <p>sixes</p>
-      <p>bonus</p>
-      <p>3 of a kind</p>
-      <p>4 of a kind</p>
-      <p>full house</p>
-      <p>small straight</p>
-      <p>large straight</p>
-      <p>yahtzee</p>
-      <p>straight</p>
+      <Category name="Aces"></Category>
+      <Category name="Twos"></Category>
+      <Category name="Threes"></Category>
+      <Category name="Fours"></Category>
+      <Category name="Fives"></Category>
+      <Category name="Sixes"></Category>
+      <Category name="Bonus"></Category>
+      <Category name="Three of a kind"></Category>
+      <Category name="Four of a kind"></Category>
+      <Category name="Full House"></Category>
+      <Category name="Small Straight"></Category>
+      <Category name="Large Straight"></Category>
+      <Category name="Yahtzee"></Category>
+      <Category name="Chance"></Category>
     </div>
   );
 }
