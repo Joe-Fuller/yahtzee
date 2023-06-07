026aa1d24757c2b250694b42524879acec2a6ef3
Joe <joefuller042@gmail.com>
2023-06-07 13:25:21 +0100

    make dice bigger

--- components/dice.js
+++ components/dice.js
@@ -5,7 +5,7 @@ export default function Dice({ value }) {
 
   return (
     <div>
-      <Image src={path} width="40" height="40" alt={value}></Image>
+      <Image src={path} width="100" height="100" alt={value}></Image>
     </div>
   );
 }
