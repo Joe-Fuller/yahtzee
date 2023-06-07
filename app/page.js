186744ac323729615e8b35991d541ccbf9666dfb
Joe <joefuller042@gmail.com>
2023-06-07 13:01:44 +0100

    make dice component work

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Scorecard from "@/components/scorecard";
+import Dice from "@/components/dice";
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-center text-3xl">YAHTZEE</h1>
       <Scorecard></Scorecard>
+      <Dice value={1}></Dice>
     </main>
   );
 }
--- components/dice.js
+++ components/dice.js
@@ -1,9 +1,11 @@
-export default function Dice(value) {
-  const path = `../images/dice/${value}.svg`;
+import Image from "next/image";
+
+export default function Dice({ value }) {
+  const path = `/dice/${value}.svg`;
 
   return (
     <div>
-      <Image src={path}></Image>
+      <Image src={path} width="40" height="40" alt={value}></Image>
     </div>
   );
 }
