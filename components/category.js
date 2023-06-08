6b67c92184d40eaf0c1cf7249649fc641e3dd197
Joe <joefuller042@gmail.com>
2023-06-08 09:50:41 +0100

    title categories nicely

--- components/category.js
+++ components/category.js
@@ -1,7 +1,19 @@
 export default function Category({ name, score, scoreCategory }) {
+  function title(word) {
+    let output = word.slice(0, 1).toUpperCase();
+    for (let i = 1; i < word.length - 1; i++) {
+      output += word[i];
+      if (word[i + 1].toUpperCase() === word[i + 1]) {
+        output += " ";
+      }
+    }
+    output += word[word.length - 1];
+    return output;
+  }
+
   return (
     <div className="grid grid-cols-2">
-      <p>{name}</p>
+      <p>{title(name)}</p>
       <p onClick={scoreCategory} id={name}>
         {score}
       </p>
